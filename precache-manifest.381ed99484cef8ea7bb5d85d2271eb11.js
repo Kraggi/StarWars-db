5d5a0c26e16d9594878734c857ee62be2a3008e8
German Yanovich <germanyanovich@gmail.com>
2020-07-22 02:40:16 +0300

    Updates

---
+++ precache-manifest.381ed99484cef8ea7bb5d85d2271eb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831424a005634d831af6aaf356f092c2",
+    "url": "/StarWars-db/index.html"
+  },
+  {
+    "revision": "e51cf2f316e5e0b825c4",
+    "url": "/StarWars-db/static/css/main.95a1496c.chunk.css"
+  },
+  {
+    "revision": "df56fd3f4b0fd708a447",
+    "url": "/StarWars-db/static/js/2.b2ea2cc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StarWars-db/static/js/2.b2ea2cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51cf2f316e5e0b825c4",
+    "url": "/StarWars-db/static/js/main.61da4111.chunk.js"
+  },
+  {
+    "revision": "6ba6db5730c9f576719c",
+    "url": "/StarWars-db/static/js/runtime-main.defbf21c.js"
+  }
+]);
