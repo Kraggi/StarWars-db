74fd380480eeeb38e418f7b8b737d70d1f3ecd30
German Yanovich <germanyanovich@gmail.com>
2020-07-21 08:30:37 +0300

    add loader spinner

--- src/components/random-planet/randomPlanet.jsx
+++ src/components/random-planet/randomPlanet.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './randomPlanet.css';
 import SwapiService from '../../services/swapi-servise';
+import Spinner from '../spinner/spinner';
 
 class RandomPlanet extends React.Component {
   constructor() {
@@ -12,10 +13,11 @@ class RandomPlanet extends React.Component {
 
   state = {
     planet: {},
+    loading: true,
   };
 
   onPlanetLoaded = planet => {
-    this.setState({ planet });
+    this.setState({ planet, loading: false });
   };
 
   updatePlanet() {
@@ -24,37 +26,40 @@ class RandomPlanet extends React.Component {
   }
 
   render() {
-    const {
-      planet: { id, name, population, rotationPeriod, diameter },
-    } = this.state;
+    const { planet, loading } = this.state;
     return (
       <div className='randomPlanet'>
         <div className='container'>
           <div className='randomPlanet_inner'>
-            <img
-              className='planet_img'
-              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-              alt='planet'
-            />
-            <div className='planet_info'>
-              <div className='planet_name'>
-                <h2>{name}</h2>
-              </div>
-              <div className='planet_param'>
-                <p className='planet_population lined'>
-                  Population: {population}
-                </p>
-                <p className='planet_rotation lined'>
-                  Rotation period: {rotationPeriod}
-                </p>
-                <p className='planet_diameter lined'>Diameter: {diameter}</p>
-              </div>
-            </div>
+            {loading ? <Spinner /> : <PlanetView planet={planet} />}
           </div>
         </div>
       </div>
     );
   }
 }
-
+const PlanetView = ({ planet }) => {
+  const { id, name, population, rotationPeriod, diameter } = planet;
+  return (
+    <>
+      <img
+        className='planet_img'
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+        alt='planet'
+      />
+      <div className='planet_info'>
+        <div className='planet_name'>
+          <h2>{name}</h2>
+        </div>
+        <div className='planet_param'>
+          <p className='planet_population lined'>Population: {population}</p>
+          <p className='planet_rotation lined'>
+            Rotation period: {rotationPeriod}
+          </p>
+          <p className='planet_diameter lined'>Diameter: {diameter}</p>
+        </div>
+      </div>
+    </>
+  );
+};
 export default RandomPlanet;
---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './spinner.css';
+function Spinner() {
+  return (
+    <div className='spinner'>
+      <div className='loadingio-spinner-double-ring-ve1fturvomr'>
+        <div className='ldio-1kvorj79mpg'>
+          <div></div>
+          <div></div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
